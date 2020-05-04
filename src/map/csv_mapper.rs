7c04b510f51eb74aa7baf0b2395c79ebe61b617c
Arthur E. Jones <PartyLich@gmail.com>
2020-05-04 17:36:44 -0400

    fix: sort map list

--- src/map/csv_mapper.rs
+++ src/map/csv_mapper.rs
@@ -5,7 +5,8 @@ pub struct CsvMapper {}
 
 impl Mapper for CsvMapper {
     fn load_map(&self) -> Result<WorldMap, Box<dyn Error>> {
-        let map_list = get_csvs_from_dir("./resource");
+        let mut map_list = get_csvs_from_dir("./resource");
+        map_list.sort();
         let file_path = map_list.get(0).ok_or("No maps found in resource dir")?;
         let content = load_to_string(file_path.to_str().ok_or("PathBuf conversion err")?);
         let grid = csv_into_vec(&content);
