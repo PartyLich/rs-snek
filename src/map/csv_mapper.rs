e04ea122ee16ed5f5c24e833754ee21e2acbaef2
Arthur E. Jones <PartyLich@gmail.com>
2020-05-13 22:17:56 -0400

    feat: select random map in Labyrinth mode

--- src/map/csv_mapper.rs
+++ src/map/csv_mapper.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 use super::*;
 
 /// A `Mapper` implementation backed by .csv file(s)
@@ -7,7 +9,10 @@ impl Mapper for CsvMapper {
     fn load_map(&self) -> Result<WorldMap, Box<dyn Error>> {
         let mut map_list = get_csvs_from_dir("./resource");
         map_list.sort();
-        let file_path = map_list.get(0).ok_or("No maps found in resource dir")?;
+        let map_selection = rand::thread_rng().gen_range(0, map_list.len());
+        let file_path = map_list
+            .get(map_selection)
+            .ok_or("No maps found in resource dir")?;
         let content = load_to_string(file_path.to_str().ok_or("PathBuf conversion err")?);
         let grid = csv_into_vec(&content);
         let walls = grid_to_position_vec(grid);
